f06372447c1d03adfd0b43e440e0a3ebef479020
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-05-03 22:47:15 +0400

    initial commit
    add some xaml markup, controls

---
+++ FakeZip/Controls/DropZone.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FakeZip.Controls
+{
+    /// <summary>
+    /// Interaction logic for DropZone.xaml
+    /// </summary>
+    public partial class DropZone : UserControl
+    {
+        public DropZone()
+        {
+            InitializeComponent();
+        }
+
+        private void DropZone_OnDrop(object sender, DragEventArgs e)
+        {
+            Console.Write("drop");
+        }
+    }
+}
---
+++ FakeZip/Controls/FileItem.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FakeZip.Controls
+{
+    /// <summary>
+    /// Interaction logic for FileItem.xaml
+    /// </summary>
+    public partial class FileItem : UserControl
+    {
+        public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register(
+            "FileName", typeof(string), typeof(FileItem));
+
+        public string FileName
+        {
+            get => (string)GetValue(FileNameProperty);
+            set => SetValue(FileNameProperty, value);
+        }
+
+        public FileItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
